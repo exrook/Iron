51d942664a10ec0b20980528b8345bfffc8116ae
Jacob Hughes <exrook.j@gmail.com>
2012-02-13 20:35:45 -0500

    Started things

--- src/main/java/org/distanthills/iron/Iron.java
+++
@@ -1,7 +0,0 @@
-package org.distanthills.iron;
-
-import org.bukkit.plugin.java.JavaPlugin;
-
-public class Iron extends JavaPlugin {
-	
-}
---
+++ src/main/java/org/distanthills/iron/IronPlugin.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package org.distanthills.iron;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author jacob
+ *
+ */
+public class IronPlugin extends JavaPlugin {
+
+	/**
+	 * 
+	 */
+	public void onEnable() {
+		
+	}
+	public void onDisable() {
+		
+	}
+}
