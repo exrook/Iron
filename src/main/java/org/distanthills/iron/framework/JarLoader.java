5cf66782772694954cdc434c634db0c488d70170
Jacob Hughes <exrook.j@gmail.com>
2012-02-23 16:29:32 -0500

    Worked on JarLoader some more, still figuring out how YAML will work

--- src/main/java/org/distanthills/iron/framework/JarLoader.java
+++ src/main/java/org/distanthills/iron/framework/JarLoader.java
@@ -7,8 +7,13 @@ import java.util.Collection;
 import java.util.LinkedList;
 import java.util.jar.JarFile;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.yaml.snakeyaml.Yaml;
 
 public class JarLoader implements ComponentLoader {
+	private static final Yaml yaml = new Yaml();
 	private URLClassLoader loader;
 	private File jardir;
 	
@@ -29,13 +34,19 @@ public class JarLoader implements ComponentLoader {
 				}
 			}
 		}
-		URL[] urls = null;
-		urls = jars.toArray(urls);
+		URL[] urls;
+		urls = jars.toArray(new URL[jars.size()]);
 		loader = new URLClassLoader(urls);
 		for (File file: files) {
 			if (file.isFile() && file.getName().toLowerCase().endsWith(".jar")) {
-				JarFile jar = new JarFile(file);
-				jar.getJarEntry("component.yml");
+				JarFile jar;
+				try {
+					jar = new JarFile(file);
+					getConfig(jar);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 				
 			}
 		}
@@ -47,6 +58,18 @@ public class JarLoader implements ComponentLoader {
 	public void unload() {
 		// TODO Auto-generated method stub
 
+	}
+	
+	private static Yaml getConfig(JarFile jar) {
+		Object dump = null;
+		try {
+			dump = yaml.load(jar.getInputStream(jar.getEntry("components.yml")));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return null;
 	}
 
 }
